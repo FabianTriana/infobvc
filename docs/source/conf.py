9078629bc84039d664df83844a5273fbe75baf98
Fabian Triana <63927438+FabianTriana@users.noreply.github.com>
2023-07-12 15:24:20 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'infobvc'
+copyright = 'Fabian Triana'
+author = 'Fabian Triana'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.0'
+version = '0.0.2'
 
 # -- General configuration
 
